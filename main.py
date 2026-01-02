17f81fceee16c8b90d2ddf058c244b16121dde65
l3afyb0y <rowenparker340@gmail.com>
2026-01-02 12:18:31 -0600

    feat: initial project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Piano Player - MIDI to audio application."""
+
+import sys
+
+def main():
+    print("Piano Player starting...")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
